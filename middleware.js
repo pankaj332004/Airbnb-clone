8e8b7abf0f41685e9d9272d3be1ad64a051485d9
Pankaj Kumar Rajbhar <pk902124@gmail.com>
2026-02-14 08:35:23 +0530

    changes done in naming

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 const Listing = require("./models/listing");
-const ExpressError = require("./utils/expressError.js");
+const ExpressError = require("./utils/ExpressError.js");
 const {listingSchema , reviewSchema} = require("./schema.js");
 const Review = require("./models/review.js")
 
