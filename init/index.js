789c420e018da56778000977213d56695bdc0b69
Pankaj Kumar Rajbhar <pk902124@gmail.com>
2026-02-14 09:38:25 +0530

    done with reinitialized

--- init/index.js
+++ init/index.js
@@ -1,8 +1,11 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+const MONGO_URL = process.env.ATLASDB_URL;
+console.log(MONGO_URL)
 
 main()
   .then(() => {
